75df407e6620945a42b2b2ce5d2ddcc4d1a150e5
Felicia <feliciamihaela2@gmail.com>
2026-01-19 00:26:24 +0200

    1.2 Implementarea middleware-urilor esențiale

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,12 @@
-import express from 'express';
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const express = require("express");
 
 const app = express();
+
+const express = require("express");
+app.use(bodyParser.json());
 
 app.get('/', (req, res)=>{
     res.send("Server is running!")
